366d90c5c80fc2e618139217d6ff0a84e3034c17
HASAN-GRDL <132680144+HASAN-GRDL@users.noreply.github.com>
2023-05-08 14:38:34 +0300

    ekran kısmında ekelem butonu aktif hale getirildi

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
 function myFunction() {
     window.open("ekran.html" ) ;
   }
+
+  function ekle() {
+
+const tr = document.createElement("tr");
+
+var userdeger = document.getElementById("isim").value;
+const user = document.createElement("th");
+const textUser = document.createTextNode(userdeger);
+user.appendChild(textUser);
+
+var datedeger = document.getElementById("date").value;
+const date = document.createElement("th");
+const textDate = document.createTextNode(datedeger );
+date.appendChild(textDate);
+
+var descriptionDeger = document.getElementById("aciklama").value;
+const description = document.createElement("th");
+const textDescription = document.createTextNode(descriptionDeger);
+description.appendChild(textDescription);
+
+tr.appendChild(user);
+tr.appendChild(date);
+tr.appendChild(description);
+document.getElementById("myTable").appendChild(tr);
+
+  }
+
+ 
+  
